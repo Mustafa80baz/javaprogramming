d5c93686bb1add2fcb7eee3cb81125876c822d4b
Mustafa80baz <112826649+Mustafa80baz@users.noreply.github.com>
2022-12-14 09:12:39 +0300

    shape dosyası

--- src/Day38CarTaskInheritance/ShapeTask/Shape.java
+++ src/Day38CarTaskInheritance/ShapeTask/Shape.java
@@ -3,7 +3,27 @@ package Day38CarTaskInheritance.ShapeTask;
 public class Shape {
    private String name;
 
-    public int area(){
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Shape(String name) {
+        if (name==null){
+            System.out.println("Name can not be null");
+            System.exit(1);// something went wrong
+        }
+        if (name.isEmpty() || name.isBlank()){
+            System.err.println("Invalid name");
+            System.exit(1);
+        }
+      setName(name);
+    }
+
+    public double area(){
         return 0;
     }
 
@@ -11,9 +31,5 @@ public class Shape {
         return 0;
     }
 
-    public String toString() {
-        return "Shape{" +
-                "name='" + name + '\'' +
-                '}';
-    }
+
 }
